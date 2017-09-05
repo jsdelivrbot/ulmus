55c0cd4c275a7be6e98b7a7c040eca41245b400a
JosephShering <joseph.shering@gmail.com>
2017-09-05 15:48:59 -0400

    Changed back to boilerplate

--- src/Main.elm
+++ src/Main.elm
@@ -3,66 +3,37 @@ module Main exposing (..)
 import Html exposing (Html, text, div, button, p, input, label)
 import Html.Events exposing (onInput, onClick)
 import Html.Attributes exposing (class)
-import Auction exposing (AuctionResponse, Auction, AuctionFile, fetchAuctions)
 import Http
 
+import Pokemon exposing (Pokemon, PokemonSprite, fetchPokemon, pokemonView)
+
 type alias Model =
-    { auctionFile : AuctionFile
-    , auctions : List Auction
-    , httpError : String
+    { helloWorld: String
     }
 
 type Msg =
-    FetchAuctions (Result Http.Error AuctionResponse)
     | NoOp
 
 initialModel : Model
 initialModel =
-    Model (AuctionFile "" 0) [] ""
+    Model "Hello World"
 
 init : (Model, Cmd Msg)
 init =
-    (initialModel, fetchAuctions FetchAuctions)
+    (initialModel, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        FetchAuctions (Ok response) ->
-            Debug.log "Good to go" |> always ({model | auctions = response.auctions}, Cmd.none)
-        FetchAuctions (Err error) ->
-            Debug.log "Error" error |> always (model, Cmd.none)
         _ ->
             (model, Cmd.none)
-
-handleHttpError : Http.Error -> String
-handleHttpError err =
-    case err of
-        Http.BadUrl str ->
-            "There was a bad url given"
-        Http.BadStatus resp ->
-            resp.status.message
-        _ ->
-            "Something went wrong"
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ p [] [ text "Currently loading auction" ]
-        , (text model.auctionFile.url)
-        , text model.httpError
+        [ text model.helloWorld
         ]
-
-renderFileUrl : Maybe AuctionFile -> Html msg
-renderFileUrl auctionFile =
-    case auctionFile of
-        Just file ->
-            div []
-                [ text file.url
-                ]
-        Nothing ->
-            text ""
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 
-const firebase = require('firebase');
 const Elm = require('./Main.elm');
-require('./stylus/index.styl');
 
-const config = {
-    apiKey: 'AIzaSyD5t66GPKV2t8lLRE70v41rKYCDAcvRR5c',
-    authDomain: 'local-life-6a815.firebaseapp.com',
-    databaseURL: 'https://local-life-6a815.firebaseio.com/',
-    storageBucket: 'gs://local-life-6a815.appspot.com/'
-};
+require('./stylus/index.styl');
 
 const mount = document.getElementById('mount');
 const app = Elm.Main.embed(mount);
